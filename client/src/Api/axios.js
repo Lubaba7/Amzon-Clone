fbd98142eb82b0d57c9b99ffe69571e7ac453deb
Saada0 <lubaba980@gmail.com>
2024-06-20 16:55:12 -0700

    firebase remote address

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-90197/us-central1/api",
+  baseURL: "https://api-5ssez3yk2q-uc.a.run.app",
 
   //deployed version of amazon server on render.com
   // baseURL: "https://amazon-api-deploy-pm1z.onrender.com",
